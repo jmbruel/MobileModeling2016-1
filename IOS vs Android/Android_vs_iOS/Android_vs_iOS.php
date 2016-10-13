2623e0b8f391be6b06b088b3012b24315b91cb40
Hamadi Sofiane <sofiane.hamadi.pro@gmail.com>
2016-10-13 11:14:01 +0200

    ajouter du dossier IOS vs Android

---
+++ IOS vs Android/Android_vs_iOS/Android_vs_iOS.php
@@ -0,0 +1,48 @@
+<!-- doctype -->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!-- page html - langue et espace de noms -->
+<html lang="fr" xmlns="http://www.w3.org/1999/xhtml">
+
+	<!-- header -->
+	<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="stylesheet" href="style.css"/>
+        <title>Android vs iOS:</title>
+    </head>
+    
+    <body>
+        <h2>Différences entre Android et iOS</h2>
+        
+        <!-- Android -->
+        <h3 id="android">Android:</h3>
+        <p id="blocTextAndroid">
+            Android est un système d'exploitation mobile 3,4 basé sur le noyau Linux et développé actuellement par Google.</br></br>
+
+            Lancé en juin 2007 à la suite du rachat par Google en 2005 de la startup du même nom, le système avait d'abord été conçu pour les smartphones et tablettes tactiles, puis s'est diversifié dans les objets connectés et ordinateurs comme les télévisions (Android TV), les voitures (Android Auto), les ordinateurs (Android-x86) et les smartwatchs (Android Wear).</br></br>
+
+            En 2015, Android est, face à iOS d'Apple, le système d'exploitation le plus utilisé dans le monde avec plus de 80 % de parts de marché dans les smartphones. (Wiki)
+        </p>
+        </br>
+        <img id="androidImg" src="android_img.jpg">
+        </br>
+    
+        <!-- iOS -->
+        <h3 id="iOS">iOS:</h3>
+        <p id="blocTextiOS">
+            iOS, anciennement iPhone OS, est le système d'exploitation mobile développé par Apple pour plusieurs de ses appareils.</br></br> 
+            
+            Il est dérivé de OS X dont il partage les fondations (le noyau hybride XNU basé sur le micro-noyau Mach, les services Unix et Cocoa, etc.). iOS comporte quatre couches d'abstraction, similaires à celles de Mac OS X : une couche «Core OS», une couche «Core Services», une couche «Media» et une couche «Cocoa». Le système d'exploitation occupe au maximum 3 Go de la capacité mémoire totale de l'appareil, selon l'appareil. (wiki)
+        </p>
+        </br>
+        <img id="iOSImg" src="iOS_img.png">
+        </br>
+
+        <!-- Tableau comparatif -->
+        <h3 id="table">Tableau comparatif</h3>
+        <iframe id="frameGoogle" src="https://docs.google.com/document/d/1nsVXGsxMOd5jSq3GGwrcH3_mM0WdA1v-WT8sGSRq-Y8/pub?embedded=true"></iframe>
+        
+    </body>
+    
+</html>
